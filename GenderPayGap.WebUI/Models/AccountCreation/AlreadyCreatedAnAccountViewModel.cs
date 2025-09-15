9a4adae6ffc9c700fd3c37f1a1d5e2d388328feb
James Griffiths <james@jwgsoftware.com>
2025-09-15 16:52:25 +0100

    EHD-1468: Update Design System library: AccountCreationController: /already-created-an-account-question

--- GenderPayGap.WebUI/Models/AccountCreation/AlreadyCreatedAnAccountViewModel.cs
+++ GenderPayGap.WebUI/Models/AccountCreation/AlreadyCreatedAnAccountViewModel.cs
@@ -1,30 +1,25 @@
-﻿using GovUkDesignSystem.Attributes;
+﻿using GovUkDesignSystemDotNet;
 
-namespace GenderPayGap.WebUI.Models.AccountCreation
+namespace GenderPayGap.WebUI.Models.AccountCreation;
+
+public class AlreadyCreatedAnAccountViewModel 
 {
 
-    public class AlreadyCreatedAnAccountViewModel 
-    {
+    [GovUkValidateRequired(ErrorMessage = "You must select whether you have already created your user account")]
+    public HaveYouAlreadyCreatedYourUserAccount? HaveYouAlreadyCreatedYourUserAccount { get; set; }
 
-        public HaveYouAlreadyCreatedYourUserAccount? HaveYouAlreadyCreatedYourUserAccount { get; set; }
+}
 
-    }
+public enum HaveYouAlreadyCreatedYourUserAccount
+{
 
-    public enum HaveYouAlreadyCreatedYourUserAccount
-    {
+    [GovUkRadioCheckboxLabelText(Text = "Yes, I have a user account")]
+    Yes = 0,
 
-        [GovUkRadioCheckboxLabelText(Text = "Yes, I have a user account")]
-        Yes = 0,
+    [GovUkRadioCheckboxLabelText(Text = "No, I don't have a user account")]
+    No = 1,
 
-        [GovUkRadioCheckboxLabelText(Text = "No, I don't have a user account")]
-        No = 1,
-
-        [GovUkRadioCheckboxLabelText(Text = "My employer has used this service before, but I haven't")]
-        NotSure = 2,
-
-        [GovUkRadioCheckboxLabelText(Text = "Unspecified")]
-        Unspecified = 3
-
-    }
+    [GovUkRadioCheckboxLabelText(Text = "My employer has used this service before, but I haven't")]
+    NotSure = 2
 
 }
