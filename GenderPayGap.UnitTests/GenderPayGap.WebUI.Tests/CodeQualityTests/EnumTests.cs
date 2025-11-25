273dcfc099f726f779b5aac4144e6eb4c4292c69
James Griffiths <james@jwgsoftware.com>
2025-11-25 17:13:31 +0000

    EHD-1977: Action Plan MVP: Make AuditLoggerTests generic to test any Enum

---
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/CodeQualityTests/EnumTests.cs
@@ -0,0 +1,39 @@
+﻿using GenderPayGap.Core;
+
+namespace GenderPayGap.BusinessLogic.Tests.Services
+{
+    [TestFixture]
+    public class EnumTests
+    {
+
+        [Test]
+        public void SomeEnumsMustHaveUniqueNumericValues()
+        {
+            EnumMustHaveUniqueValues<AuditedAction, int>();
+        }
+
+        private static void EnumMustHaveUniqueValues<TEnum, TNumeric>()
+        where TEnum : Enum
+        {
+            List<TEnum> enumOptions = Enum.GetValues(typeof(TEnum))
+                .Cast<TEnum>()
+                .ToList();
+
+            foreach (TEnum enumOption in enumOptions)
+            {
+                TNumeric numericValueOfEnumOption = (TNumeric)(object) enumOption;
+
+                int numberOfEnumOptionsWithThisNumericValue = enumOptions
+                    .Select(eo => (TNumeric)(object) eo)
+                    .Count(eo => eo.Equals(numericValueOfEnumOption));
+
+                Assert.AreEqual(
+                    1,
+                    numberOfEnumOptionsWithThisNumericValue,
+                    $"{typeof(TEnum)} enum has {numberOfEnumOptionsWithThisNumericValue} options with the numerical "
+                    + $"value {numericValueOfEnumOption}. The numerical values of an enum must be unique.");
+            }
+        }
+
+    }
+}
