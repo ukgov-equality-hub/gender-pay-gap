dcab607df16230af9a2b2c93183881dd94cac97d
James Griffiths <james@jwgsoftware.com>
2025-12-16 12:04:30 +0000

    EHD-1976: Action Plan MVP: Employer journey: Intro page

---
+++ GenderPayGap.WebUI/Controllers/ActionPlans/ActionPlanController.cs
@@ -0,0 +1,40 @@
+﻿using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.WebUI.Helpers;
+using GenderPayGap.WebUI.Models.ActionPlans;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.ActionPlans;
+
+[Authorize(Roles = LoginRoles.GpgEmployer)]
+[Route("account/organisations")]
+public class ActionPlanController: Controller
+{
+    private readonly IDataRepository dataRepository;
+    
+    public ActionPlanController(IDataRepository dataRepository)
+    {
+        this.dataRepository = dataRepository;
+    }
+
+    [HttpGet("{encryptedOrganisationId}/reporting-year-{reportingYear}/action-plan/intro")]
+    public IActionResult ActionPlanIntroGet(string encryptedOrganisationId, int reportingYear)
+    {
+        long organisationId = ControllerHelper.DecryptOrganisationIdOrThrow404(encryptedOrganisationId);
+        ControllerHelper.ThrowIfUserAccountRetiredOrEmailNotVerified(User, dataRepository);
+        ControllerHelper.ThrowIfUserDoesNotHavePermissionsForGivenOrganisation(User, dataRepository, organisationId);
+        ControllerHelper.ThrowIfReportingYearIsOutsideOfRange(reportingYear, organisationId, dataRepository);
+        
+        Organisation organisation = dataRepository.Get<Organisation>(organisationId);
+        
+        ActionPlanIntroViewModel viewModel = new ActionPlanIntroViewModel
+        {
+            Organisation = organisation,
+            ReportingYear = reportingYear
+        };
+        
+        return View("ActionPlanIntro", viewModel);
+    }
+
+}
