257e9fcef350755ac4a54648caa1796ab9d83399
James Griffiths <james@jwgsoftware.com>
2026-01-15 10:04:55 +0000

    EHD-2012: Employer Dashboard: Manage Organisation page

--- GenderPayGap.WebUI/Models/ManageOrganisations/ManageOrganisationViewModel.cs
+++ GenderPayGap.WebUI/Models/ManageOrganisations/ManageOrganisationViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using GenderPayGap.Core;
+using GenderPayGap.Core.Helpers;
 using GenderPayGap.Database;
 using GenderPayGap.Database.Models;
+using GenderPayGap.Extensions;
+using GenderPayGap.WebUI.Helpers;
+using GovUkDesignSystemDotNet;
 
 namespace GenderPayGap.WebUI.Models.ManageOrganisations
 {
@@ -36,38 +40,18 @@ namespace GenderPayGap.WebUI.Models.ManageOrganisations
             return users;
         }
 
-        public bool DoesReturnOrDraftReturnExistForYear(int reportingYear)
+        public bool HasDraftGpgReport(int reportingYear)
         {
-            bool hasReturnForYear = Organisation.HasSubmittedReturn(reportingYear);
-            bool hasDraftReturnForYear = allDraftReturns.Any(d => d.SnapshotYear == reportingYear);
-
-            return hasReturnForYear || hasDraftReturnForYear;
-        }
-        
-        public string GetReportLinkText(int reportingYear)
-        {
-            bool hasReturnForYear = Organisation.HasSubmittedReturn(reportingYear);
-            bool hasDraftReturnForYear = allDraftReturns.Any(d => d.SnapshotYear == reportingYear);
-
-            if (!hasReturnForYear && !hasDraftReturnForYear)
-            {
-                return "Draft report";
-            }
-
-            if (!hasReturnForYear && hasDraftReturnForYear)
-            {
-                return "Edit draft";
-            }
-
-            if (hasReturnForYear && !hasDraftReturnForYear)
-            {
-                return "Edit report";
-            }
-
-            return "Edit draft report";
+            return allDraftReturns.Any(d => d.SnapshotYear == reportingYear);
         }
 
         public bool OrganisationIsRequiredToSubmit(int reportingYear)
+        {
+            return OrganisationIsIsScope(reportingYear)
+                   && !Global.ReportingStartYearsToExcludeFromLateFlagEnforcement.Contains(reportingYear);
+        }
+
+        public bool OrganisationIsIsScope(int reportingYear)
         {
             OrganisationScope scopeForYear = Organisation.GetScopeForYear(reportingYear);
             if (scopeForYear == null)
@@ -75,8 +59,256 @@ namespace GenderPayGap.WebUI.Models.ManageOrganisations
                 return true;
             }
             
-            return scopeForYear.IsInScopeVariant()
-                   && !Global.ReportingStartYearsToExcludeFromLateFlagEnforcement.Contains(reportingYear);
+            return scopeForYear.IsInScopeVariant();
+        }
+
+        public bool ShowScopeDeclarationStatus(int reportingYear)
+        {
+            return RequiredToDeclareScope(reportingYear) || IsScopeDeclared(reportingYear);
+        }
+
+        public TagViewModel GetScopeDeclarationStatusTag(int reportingYear)
+        {
+            if (IsScopeDeclared(reportingYear))
+            {
+                return new TagViewModel
+                {
+                    HtmlOrText = new("Completed"),
+                    Classes = ["govuk-tag--green"]
+                };
+            }
+            else if (DeadlineHasPassed(reportingYear))
+            {
+                return new TagViewModel
+                {
+                    HtmlOrText = new("Overdue"),
+                    Classes = ["govuk-tag--red"]
+                };
+            }
+            else
+            {
+                return new TagViewModel
+                {
+                    HtmlOrText = new("Not started"),
+                    Classes = ["govuk-tag--blue"]
+                };
+            }
+        }
+        
+        public bool ShowScopeStatus(int reportingYear)
+        {
+            return IsScopeDeclared(reportingYear) || !RequiredToDeclareScope(reportingYear);
+        }
+
+        public TagViewModel GetScopeStatusTag(int reportingYear)
+        {
+            string scopeStatusText = Organisation.GetScopeStatusForYear(reportingYear).IsInScopeVariant()
+                ? "Required to report"
+                : "Not required to report";
+            
+            return new TagViewModel
+            {
+                HtmlOrText = new(scopeStatusText),
+                Classes = ["govuk-tag--grey"],
+                Attributes = new() {{"style", "max-width: unset;"}},
+            };
+        }
+
+        private bool IsScopeDeclared(int reportingYear) => Organisation.GetScopeStatusForYear(reportingYear).IsScopeDeclared();
+
+        private static bool RequiredToDeclareScope(int reportingYear) => reportingYear >= Global.FirstReportingYearForMandatoryScopeDeclaration;
+
+        private bool DeadlineHasPassed(int reportingYear) => ReportingYearsHelper.DeadlineHasPassedForYearAndSector(reportingYear, Organisation.SectorType);
+
+        public bool ShowCovidMessage(int reportingYear)
+        {
+            bool inScopeForYear = OrganisationIsIsScope(reportingYear);
+            ReportStatusTag reportStatusTag = ReportStatusTagHelper.GetReportStatusTag(Organisation, reportingYear);
+
+            return inScopeForYear
+                   && ReportingYearsHelper.IsReportingYearExcludedFromLateFlagEnforcement(reportingYear)
+                   && (reportStatusTag == ReportStatusTag.NotRequiredDueToCovid ||
+                       reportStatusTag == ReportStatusTag.Overdue ||
+                       reportStatusTag == ReportStatusTag.SubmittedLate);
+        }
+        
+        public bool ShowActionPlanRow(int reportingYear)
+        {
+            return reportingYear >= Global.FirstReportingYearForActionPlans;
+        }
+
+        public TagViewModel ActionPlanStatusTag(int reportingYear)
+        {
+            if (!AbleToCreateActionPlan(reportingYear))
+            {
+                return new TagViewModel
+                {
+                    HtmlOrText = new("Cannot start yet"),
+                    Classes = ["govuk-tag--grey"],
+                    Attributes = new() {{"style", "max-width: unset;"}},
+                };
+            }
+            else if (Organisation.GetLatestSubmittedActionPlan(reportingYear) != null)
+            {
+                string statusText = "Submitted";
+                if (Organisation.GetLatestSubmittedOrDraftActionPlan(reportingYear).Status == ActionPlanStatus.Draft)
+                {
+                    statusText += ", draft amendments";
+                }
+
+                return new TagViewModel
+                {
+                    HtmlOrText = new(statusText),
+                    Classes = ["govuk-tag--green"],
+                    Attributes = new() {{"style", "max-width: unset;"}},
+                };
+            }
+            else if (Organisation.GetLatestSubmittedOrDraftActionPlan(reportingYear) != null)
+            {
+                return new TagViewModel
+                {
+                    HtmlOrText = new("Draft created, not submitted"),
+                    Classes = ["govuk-tag--blue"],
+                    Attributes = new() {{"style", "max-width: unset;"}},
+                };
+            }
+            else
+            {
+                return new TagViewModel
+                {
+                    HtmlOrText = new("Not started"),
+                    Classes = ["govuk-tag--blue"],
+                    Attributes = new() {{"style", "max-width: unset;"}},
+                };
+            }
+        }
+
+        public bool IsActionPlanOptional(int reportingYear)
+        {
+            return reportingYear >= Global.FirstReportingYearForActionPlans
+                   && reportingYear < Global.FirstReportingYearForMandatoryActionPlans;
+        }
+
+        public bool AbleToReportGpg(int reportingYear)
+        {
+            return IsScopeDeclared(reportingYear) || !RequiredToDeclareScope(reportingYear);
+        }
+
+        public bool AbleToCreateActionPlan(int reportingYear)
+        {
+            return IsScopeDeclared(reportingYear) || !RequiredToDeclareScope(reportingYear);
+        }
+
+        private bool AllReportingCompleteForYear(int reportingYear)
+        {
+            if (RequiredToDeclareScope(reportingYear) && !IsScopeDeclared(reportingYear))
+            {
+                return false;
+            }
+
+            if (!OrganisationIsRequiredToSubmit(reportingYear))
+            {
+                return true;
+            }
+
+            return !GpgReportingIncomplete(reportingYear) && !ActionPlanReportingIncomplete(reportingYear);
+        }
+
+        private bool GpgReportingIncomplete(int reportingYear)
+        {
+            return !Organisation.HasSubmittedReturn(reportingYear);
+        }
+
+        private bool ActionPlanReportingIncomplete(int reportingYear)
+        {
+            if (!ShowActionPlanRow(reportingYear))
+            {
+                return false;
+            }
+
+            if (IsActionPlanOptional(reportingYear))
+            {
+                return false;
+            }
+
+            return Organisation.GetLatestSubmittedActionPlan(reportingYear) == null;
+        }
+
+        public string GetDeadlineDateFormatted(int reportingYear)
+        {
+            DateTime deadlineDate = ReportingYearsHelper.GetDeadline(Organisation.SectorType, reportingYear);
+            return deadlineDate.ToString("d MMMM yyyy");
+        }
+
+        public string GetDeadlineRelativeDaysIfNeeded(int reportingYear)
+        {
+            DateTime deadlineDate = ReportingYearsHelper.GetDeadline(Organisation.SectorType, reportingYear);
+            DateTime today = VirtualDateTime.Now.Date;
+            
+            int currentReportingYear = ReportingYearsHelper.GetCurrentReportingYear(Organisation.SectorType);
+            bool isCurrentReportingYear = reportingYear == currentReportingYear;
+            bool isPreviousReportingYear = reportingYear == currentReportingYear - 1;
+            
+            if (isCurrentReportingYear || isPreviousReportingYear)
+            {
+                if (deadlineDate < today)
+                {
+                    int daysPastDeadline = (int) ((today - deadlineDate).TotalDays);
+                    return $"({daysPastDeadline} {(daysPastDeadline == 1 ? "day" : "days")} ago)";
+                }
+                else if (deadlineDate == today)
+                {
+                    return "(today)";
+                }
+                else
+                {
+                    int daysUntilDeadline = (int) ((deadlineDate - today).TotalDays);
+                    return $"({daysUntilDeadline} {(daysUntilDeadline == 1 ? "day" : "days")} until the deadline)";
+                }
+            }
+
+            return null;
+        }
+
+        public TagViewModel GetOverallStatusTagForYear(int reportingYear)
+        {
+            DateTime deadlineDate = ReportingYearsHelper.GetDeadline(Organisation.SectorType, reportingYear);
+            DateTime today = VirtualDateTime.Now.Date;
+            
+            int currentReportingYear = ReportingYearsHelper.GetCurrentReportingYear(Organisation.SectorType);
+            bool isCurrentReportingYear = reportingYear == currentReportingYear;
+            
+            if (AllReportingCompleteForYear(reportingYear))
+            {
+                return new TagViewModel
+                {
+                    HtmlOrText = new("Complete"),
+                    Classes = ["govuk-tag--green"],
+                    Attributes = new(){{"style", "max-width: unset;"}},
+                };
+            }
+            else if (isCurrentReportingYear)
+            {
+                int daysUntilDeadline = (int) ((deadlineDate - today).TotalDays);
+
+                return new TagViewModel
+                {
+                    HtmlOrText = new($"Due: You have {daysUntilDeadline} {(daysUntilDeadline == 1 ? "day" : "days")} until the deadline"),
+                    Classes = ["govuk-tag--blue"],
+                    Attributes = new(){{"style", "max-width: unset;"}},
+                };
+            }
+            else
+            {
+                int daysOverdue = (int) ((today - deadlineDate).TotalDays);
+                
+                return new TagViewModel
+                {
+                    HtmlOrText = new($"Overdue: your reports are {daysOverdue} {(daysOverdue == 1 ? "day" : "days")} overdue"),
+                    Classes = ["govuk-tag--red"],
+                    Attributes = new(){{"style", "max-width: unset;"}},
+                };
+            }
         }
 
     }
