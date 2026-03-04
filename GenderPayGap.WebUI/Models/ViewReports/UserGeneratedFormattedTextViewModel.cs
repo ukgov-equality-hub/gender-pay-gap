8230c1098a21396cede8d1c856b51690393651ca
James Griffiths <james@jwgsoftware.com>
2026-03-04 11:14:09 +0000

    EHD-2046: Action Plans: Fixes: Allow formatted text (just paragraphs for now) in Supporting Narrative and Supporting Text

---
+++ GenderPayGap.WebUI/Models/ViewReports/UserGeneratedFormattedTextViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace GenderPayGap.WebUI.Models.ViewReports;
+
+public class UserGeneratedFormattedTextViewModel
+{
+
+    private readonly string text;
+
+    public UserGeneratedFormattedTextViewModel(string text)
+    {
+        this.text = text;
+    }
+
+    public List<string> GetParagraphs()
+    {
+        if (text == null)
+        {
+            return [];
+        }
+        
+        return text.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
+
+}
