8fe419a958fb5ac1d02be2c602fe3a1566c4ca3b
James Griffiths <james@jwgsoftware.com>
2026-03-05 13:29:38 +0000

    EHD-2046: Action Plans: Fixes: Character Count: Late Submission Reason page

--- GenderPayGap.WebUI/Models/Report/LateSubmissionReasonViewModel.cs
+++ GenderPayGap.WebUI/Models/Report/LateSubmissionReasonViewModel.cs
@@ -1,36 +1,35 @@
 ﻿using GenderPayGap.Database;
-using GovUkDesignSystem.Attributes.ValidationAttributes;
+using GovUkDesignSystemDotNet;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace GenderPayGap.WebUI.Models.Report
+namespace GenderPayGap.WebUI.Models.Report;
+
+public class LateSubmissionReasonViewModel 
 {
-    public class LateSubmissionReasonViewModel 
-    {
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public Organisation Organisation { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public Organisation Organisation { get; set; }
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public int ReportingYear { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public int ReportingYear { get; set; }
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public bool IsEditingSubmittedReturn { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public bool IsEditingSubmittedReturn { get; set; }
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public DateTime DeadlineDate { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public DateTime DeadlineDate { get; set; }
 
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Select yes if you have received a letter about a breach of the regulations from the Equality and Human Rights Commission.")]
-        public ReportLateSubmissionReceivedLetterFromEhrc? ReceivedLetterFromEhrc { get; set; }
+    [GovUkValidateRequired(ErrorMessageIfMissing = "Select yes if you have received a letter about a breach of the regulations from the Equality and Human Rights Commission.")]
+    public ReportLateSubmissionReceivedLetterFromEhrc? ReceivedLetterFromEhrc { get; set; }
         
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Explain why your organisation is reporting or changing your gender pay gap figures after the deadline.")]
-        [GovUkValidateCharacterCount(MaxCharacters = 200, NameAtStartOfSentence = "Reason", NameWithinSentence = "reason")]
-        public string Reason { get; set; }
+    [GovUkValidateRequired(ErrorMessageIfMissing = "Explain why your organisation is reporting or changing your gender pay gap figures after the deadline.")]
+    [GovUkValidateCharacterCount(Limit = 200, Units = CharacterCountMaxLengthUnit.Characters, NameAtStartOfSentence = "Reason", NameWithinSentence = "reason")]
+    public string Reason { get; set; }
 
-    }
+}
 
-    public enum ReportLateSubmissionReceivedLetterFromEhrc
-    {
-        Yes,
-        No
-    }
+public enum ReportLateSubmissionReceivedLetterFromEhrc
+{
+    Yes,
+    No
 }
