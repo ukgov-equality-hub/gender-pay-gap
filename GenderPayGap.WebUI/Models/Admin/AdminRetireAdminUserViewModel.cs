65f429da82761ee5f61219f63178e3a14a2728f6
James Griffiths <james@jwgsoftware.com>
2026-03-09 14:20:07 +0000

    EHD-2046: Action Plans: Fixes: Character Count: Admin Retire Admin User page

--- GenderPayGap.WebUI/Models/Admin/AdminRetireAdminUserViewModel.cs
+++ GenderPayGap.WebUI/Models/Admin/AdminRetireAdminUserViewModel.cs
@@ -1,18 +1,17 @@
 using GenderPayGap.Database;
-using GovUkDesignSystem.Attributes.ValidationAttributes;
+using GovUkDesignSystemDotNet;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace GenderPayGap.WebUI.Models.Admin
+namespace GenderPayGap.WebUI.Models.Admin;
+
+public class AdminRetireAdminUserViewModel 
 {
-    public class AdminRetireAdminUserViewModel 
-    {
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public User User { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public User User { get; set; }
 
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
-        [GovUkValidateCharacterCount(MaxCharacters = 250, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
-        public string Reason { get; set; }
+    [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
+    [GovUkValidateCharacterCount(Limit = 250, Units = CharacterCountMaxLengthUnit.Characters, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
+    public string Reason { get; set; }
 
-    }
 }
