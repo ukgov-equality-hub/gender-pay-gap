353454601f71cc9ef3e1cf80100482514acabcd3
James Griffiths <james@jwgsoftware.com>
2026-03-09 15:59:37 +0000

    EHD-2046: Action Plans: Fixes: Character Count: Admin Change Organisation Name pages

--- GenderPayGap.WebUI/Models/Admin/ChangeOrganisationNameViewModel.cs
+++ GenderPayGap.WebUI/Models/Admin/ChangeOrganisationNameViewModel.cs
@@ -1,60 +1,58 @@
 ﻿using GenderPayGap.Database;
-using GovUkDesignSystem.Attributes;
-using GovUkDesignSystem.Attributes.ValidationAttributes;
+using GovUkDesignSystemDotNet;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace GenderPayGap.WebUI.Models.Admin
+namespace GenderPayGap.WebUI.Models.Admin;
+
+public class ChangeOrganisationNameViewModel 
 {
-    public class ChangeOrganisationNameViewModel 
-    {
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public Organisation Organisation { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public Organisation Organisation { get; set; }
 
-        public ManuallyChangeOrganisationNameViewModelActions Action { get; set; }
+    public ManuallyChangeOrganisationNameViewModelActions Action { get; set; }
 
-        [GovUkValidateRequiredIf(
-            IsRequiredPropertyName = nameof(NameRequired),
-            ErrorMessageIfMissing = "Please enter a name")]
-        public string Name { get; set; }
+    [GovUkValidateRequiredIf(
+        IsRequiredPropertyName = nameof(NameRequired),
+        ErrorMessageIfMissing = "Please enter a name")]
+    public string Name { get; set; }
 
-        public bool NameRequired => Action is ManuallyChangeOrganisationNameViewModelActions.ManualChange;
+    public bool NameRequired => Action is ManuallyChangeOrganisationNameViewModelActions.ManualChange;
 
-        [GovUkValidateRequiredIf(
-            IsRequiredPropertyName = nameof(AcceptCompaniesHouseNameRequired),
-            ErrorMessageIfMissing =
+    [GovUkValidateRequiredIf(
+        IsRequiredPropertyName = nameof(AcceptCompaniesHouseNameRequired),
+        ErrorMessageIfMissing =
             "Select whether you want to use this name from Companies House or to enter a name manually")]
-        public AcceptCompaniesHouseName? AcceptCompaniesHouseName { get; set; }
+    public AcceptCompaniesHouseName? AcceptCompaniesHouseName { get; set; }
 
-        public bool AcceptCompaniesHouseNameRequired => Action is ManuallyChangeOrganisationNameViewModelActions.OfferNewCompaniesHouseName;
-        [GovUkValidateRequiredIf(
-            IsRequiredPropertyName = nameof(ReasonRequired),
-            ErrorMessageIfMissing = "Please enter a reason for this change")]
-        [GovUkValidateCharacterCount(MaxCharacters = 250, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
-        public string Reason { get; set; }
+    public bool AcceptCompaniesHouseNameRequired => Action is ManuallyChangeOrganisationNameViewModelActions.OfferNewCompaniesHouseName;
+    [GovUkValidateRequiredIf(
+        IsRequiredPropertyName = nameof(ReasonRequired),
+        ErrorMessageIfMissing = "Please enter a reason for this change")]
+    [GovUkValidateCharacterCount(Limit = 250, Units = CharacterCountMaxLengthUnit.Characters, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
+    public string Reason { get; set; }
 
-        public bool ReasonRequired =>
-            (Action is ManuallyChangeOrganisationNameViewModelActions.OfferNewCompaniesHouseName && 
-             AcceptCompaniesHouseName is Admin.AcceptCompaniesHouseName.Accept) || 
-            Action is ManuallyChangeOrganisationNameViewModelActions.ManualChange;
+    public bool ReasonRequired =>
+        (Action is ManuallyChangeOrganisationNameViewModelActions.OfferNewCompaniesHouseName && 
+         AcceptCompaniesHouseName is Admin.AcceptCompaniesHouseName.Accept) || 
+        Action is ManuallyChangeOrganisationNameViewModelActions.ManualChange;
 
-    }
+}
 
-    public enum AcceptCompaniesHouseName
-    {
-        [GovUkRadioCheckboxLabelText(Text = "Yes, use this name from Companies House")]
-        Accept,
+public enum AcceptCompaniesHouseName
+{
+    [GovUkRadioCheckboxLabelText(Text = "Yes, use this name from Companies House")]
+    Accept,
 
-        [GovUkRadioCheckboxLabelText(Text = "No, enter a name manually")]
-        Reject
-    }
+    [GovUkRadioCheckboxLabelText(Text = "No, enter a name manually")]
+    Reject
+}
 
-    public enum ManuallyChangeOrganisationNameViewModelActions
-    {
-        Unknown = 0,
-        OfferNewCompaniesHouseName = 1,
-        ManualChange = 2,
-        CheckChangesManual = 3,
-        CheckChangesCoHo = 4
-    }
+public enum ManuallyChangeOrganisationNameViewModelActions
+{
+    Unknown = 0,
+    OfferNewCompaniesHouseName = 1,
+    ManualChange = 2,
+    CheckChangesManual = 3,
+    CheckChangesCoHo = 4
 }
