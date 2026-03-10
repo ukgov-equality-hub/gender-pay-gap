5e0546d422171b650b4f78f9b60f6dafe4d9b84a
James Griffiths <james@jwgsoftware.com>
2026-03-10 11:59:43 +0000

    EHD-2046: Action Plans: Fixes: Character Count: Admin Change Late Flag page

--- GenderPayGap.WebUI/Models/Admin/AdminReturnLateFlagViewModel.cs
+++ GenderPayGap.WebUI/Models/Admin/AdminReturnLateFlagViewModel.cs
@@ -1,20 +1,19 @@
 using GenderPayGap.Database;
-using GovUkDesignSystem.Attributes.ValidationAttributes;
+using GovUkDesignSystemDotNet;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace GenderPayGap.WebUI.Models.Admin
+namespace GenderPayGap.WebUI.Models.Admin;
+
+public class AdminReturnLateFlagViewModel 
 {
-    public class AdminReturnLateFlagViewModel 
-    {
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public Return Return { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public Return Return { get; set; }
 
-        public bool? NewLateFlag { set; get; }
+    public bool? NewLateFlag { set; get; }
 
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
-        [GovUkValidateCharacterCount(MaxCharacters = 250, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
-        public string Reason { get; set; }
+    [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
+    [GovUkValidateCharacterCount(Limit = 250, Units = CharacterCountMaxLengthUnit.Characters, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
+    public string Reason { get; set; }
 
-    }
 }
