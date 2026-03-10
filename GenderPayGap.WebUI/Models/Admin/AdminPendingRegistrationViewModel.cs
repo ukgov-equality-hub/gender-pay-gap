d5e293fc88877214c518ab099d8a6c57d6c1db5f
James Griffiths <james@jwgsoftware.com>
2026-03-10 11:33:04 +0000

    EHD-2046: Action Plans: Fixes: Character Count: Admin Pending Registration page

--- GenderPayGap.WebUI/Models/Admin/AdminPendingRegistrationViewModel.cs
+++ GenderPayGap.WebUI/Models/Admin/AdminPendingRegistrationViewModel.cs
@@ -1,38 +1,35 @@
 ﻿using GenderPayGap.Database;
 using GenderPayGap.WebUI.Models.AddOrganisation;
-using GovUkDesignSystem.Attributes.ValidationAttributes;
+using GovUkDesignSystemDotNet;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace GenderPayGap.WebUI.Models.Admin
+namespace GenderPayGap.WebUI.Models.Admin;
+
+public class AdminPendingRegistrationViewModel
 {
-    public class AdminPendingRegistrationViewModel
-    {
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public UserOrganisation UserOrganisation { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public UserOrganisation UserOrganisation { get; set; }
 
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public List<AddOrganisationSearchResult> SimilarOrganisationsFromOurDatabase { get; set; }
-        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
-        public List<AddOrganisationSearchResult> SimilarOrganisationsFromCompaniesHouse { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public List<AddOrganisationSearchResult> SimilarOrganisationsFromOurDatabase { get; set; }
+    [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+    public List<AddOrganisationSearchResult> SimilarOrganisationsFromCompaniesHouse { get; set; }
 
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Select whether to approve or reject this registration")]
-        public AdminPendingRegistrationApproveOrReject? ApproveOrReject { get; set; }
+    [GovUkValidateRequired(ErrorMessageIfMissing = "Select whether to approve or reject this registration")]
+    public AdminPendingRegistrationApproveOrReject? ApproveOrReject { get; set; }
 
-        [GovUkValidateRequiredIf(
-            IsRequiredPropertyName = nameof(RejectionReasonRequired),
-            ErrorMessageIfMissing = "Enter a reason for rejecting this registration")]
-        [GovUkValidateCharacterCount(MaxCharacters = 250, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
-        public string RejectionReason { get; set; }
-        public bool RejectionReasonRequired => ApproveOrReject == AdminPendingRegistrationApproveOrReject.Reject;
+    [GovUkValidateRequiredIf(
+        IsRequiredPropertyName = nameof(RejectionReasonRequired),
+        ErrorMessageIfMissing = "Enter a reason for rejecting this registration")]
+    [GovUkValidateCharacterCount(Limit = 250, Units = CharacterCountMaxLengthUnit.Characters, NameAtStartOfSentence = "Reason", NameWithinSentence = "Reason")]
+    public string RejectionReason { get; set; }
+    public bool RejectionReasonRequired => ApproveOrReject == AdminPendingRegistrationApproveOrReject.Reject;
         
-    }
+}
     
-    public enum AdminPendingRegistrationApproveOrReject
-    {
-
-        Approve,
-        Reject
-
-    }
+public enum AdminPendingRegistrationApproveOrReject
+{
+    Approve,
+    Reject
 }
