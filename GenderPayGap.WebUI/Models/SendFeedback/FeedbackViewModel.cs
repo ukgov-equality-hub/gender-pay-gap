93160d959328bcfbb1618d7b2cab2216d93d5b25
James Griffiths <james@jwgsoftware.com>
2026-04-09 18:12:07 +0100

    EHD-2137: Action Plans: Feedback form: Code changes

--- GenderPayGap.WebUI/Models/SendFeedback/FeedbackViewModel.cs
+++ GenderPayGap.WebUI/Models/SendFeedback/FeedbackViewModel.cs
@@ -4,8 +4,11 @@ namespace GenderPayGap.WebUI.Models.SendFeedback;
 
 public class FeedbackViewModel 
 {
-    [GovUkValidateRequired(ErrorMessageIfMissing = "Select how easy this service is to use.")]
-    public HowEasyIsThisServiceToUse? HowEasyIsThisServiceToUse { get; set; }
+    [GovUkValidateRequired(ErrorMessageIfMissing = "Select how easy it was to submit your gender pay gap data")]
+    public HowEasyWasItToSubmitYourGenderPayGapData? HowEasyWasItToSubmitYourGenderPayGapData { get; set; }
+
+    [GovUkValidateRequired(ErrorMessageIfMissing = "Select how easy it was to create your action plan")]
+    public HowEasyWasItToCreateYourActionPlan? HowEasyWasItToCreateYourActionPlan { get; set; }
 
     public List<HowDidYouHearAboutGpg> HowDidYouHearAboutGpg { get; set; } = [];
 
@@ -25,14 +28,14 @@ public class FeedbackViewModel
     [GovUkValidateCharacterCount(Limit = 2000, Units = CharacterCountMaxLengthUnit.Characters, NameAtStartOfSentence = "Details", NameWithinSentence = "details")]
     public string Details { get; set; }
 
-    public string EmailAddress { get; set; }
+    public string YourName { get; set; }
 
-    public string PhoneNumber { get; set; }
+    public string EmailAddress { get; set; }
 
 }
 
     
-public enum HowEasyIsThisServiceToUse
+public enum HowEasyWasItToSubmitYourGenderPayGapData
 {
 
     [GovUkRadioCheckboxLabelText(Text = "Very easy")]
@@ -48,7 +51,33 @@ public enum HowEasyIsThisServiceToUse
     Difficult = 3,
 
     [GovUkRadioCheckboxLabelText(Text = "Very difficult")]
-    VeryDifficult = 4
+    VeryDifficult = 4,
+
+    [GovUkRadioCheckboxLabelText(Text = "Not applicable")]
+    NotApplicable = 5,
+
+}
+
+public enum HowEasyWasItToCreateYourActionPlan
+{
+
+    [GovUkRadioCheckboxLabelText(Text = "Very easy")]
+    VeryEasy = 0,
+
+    [GovUkRadioCheckboxLabelText(Text = "Easy")]
+    Easy = 1,
+
+    [GovUkRadioCheckboxLabelText(Text = "Neither easy nor difficult")]
+    Neutral = 2,
+
+    [GovUkRadioCheckboxLabelText(Text = "Difficult")]
+    Difficult = 3,
+
+    [GovUkRadioCheckboxLabelText(Text = "Very difficult")]
+    VeryDifficult = 4,
+
+    [GovUkRadioCheckboxLabelText(Text = "Not applicable")]
+    NotApplicable = 5,
 
 }
 
@@ -90,17 +119,23 @@ public enum WhyVisitGpgSite
     [GovUkRadioCheckboxLabelText(Text = "Find out more about the gender pay gap")]
     FindOutAboutGpg,
 
-    [GovUkRadioCheckboxLabelText(Text = "Submit data for my organisation")]
+    [GovUkRadioCheckboxLabelText(Text = "Submit gender pay gap data for my organisation")]
     ReportOrganisationGpgData,
 
     [GovUkRadioCheckboxLabelText(Text = "Find out how my organisation can close its gap")]
     CloseOrganisationGpg,
 
-    [GovUkRadioCheckboxLabelText(Text = "Look at gender pay gap data for specific organisations")]
+    [GovUkRadioCheckboxLabelText(Text = "Look at gender pay gap data for specific organisations or sectors")]
     ViewSpecificOrganisationGpg,
 
-    [GovUkRadioCheckboxLabelText(Text = "See what other organisations are doing to close their gap")]
-    ActionsToCloseGpg,
+    [GovUkRadioCheckboxLabelText(Text = "Find out more about creating an action plan")]
+    FindOutMoreAboutCreatingAnActionPlan,
+
+    [GovUkRadioCheckboxLabelText(Text = "Create an action plan for my organisation")]
+    CreateAnActionPlanForMyOrganisation,
+
+    [GovUkRadioCheckboxLabelText(Text = "Look at action plans for specific organisations or sectors")]
+    LookAtActionPlansForOrganisationsOrSectors,
 
     [GovUkRadioCheckboxLabelText(Text = "Other")]
     Other
@@ -113,8 +148,14 @@ public enum WhoAreYou
     [GovUkRadioCheckboxLabelText(Text = "Employee interested in my organisation’s gender pay gap data")]
     EmployeeInterestedInOrganisationData,
 
-    [GovUkRadioCheckboxLabelText(Text = "Employee responsible for submitting my organisation’s data")]
+    [GovUkRadioCheckboxLabelText(Text = "Employee interested in my organisation’s action plan")]
+    EmployeeInterestedInOrganisationActionPlan,
+
+    [GovUkRadioCheckboxLabelText(Text = "Employee responsible for submitting my organisation’s gender pay gap data")]
     ResponsibleForReportingGpg,
+
+    [GovUkRadioCheckboxLabelText(Text = "Employee responsible for submitting my organisation’s action plan")]
+    EmployeeResponsibleForSubmittingActionPlan,
 
     [GovUkRadioCheckboxLabelText(Text = "Manager involved in gender pay gap reporting or diversity and inclusion")]
     ManagerInvolvedInGpgReport,
